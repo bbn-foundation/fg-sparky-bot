51d4132bba1e124d81963165e11e7f201602e237
Skylafalls <triplettran56@gmail.com>
2025-12-20 20:30:22 -0500

    fix(numberdex/add): use same handler
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- src/commands/numberdex.ts
+++ src/commands/numberdex.ts
@@ -4,9 +4,13 @@
  * Copyright (C) 2025 Skylafalls
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
-import { ApplicationCommandOptionType, ChannelType, type Client, type CommandInteraction } from "discord.js";
+import { ApplicationCommandOptionType, ChannelType, type Client, type CommandInteraction, type Message, type OmitPartialGroupDMChannel } from "discord.js";
+import { handlePlayerGuess } from "../numberdex/handler.ts";
 import { baker } from "../numberdex/index.ts";
+import { spawnNumberhuman } from "../numberdex/utils.ts";
 import { Logger } from "../utils/logger.ts";
+import { joinStringArray } from "../utils/string.ts";
+import { createUser, getUser } from "../utils/user.ts";
 import type { Command } from "./types.ts";
 
 const Numberdex: Command = {
@@ -24,7 +28,55 @@ const Numberdex: Command = {
           cron: "@every_20_minutes",
           async callback(): Promise<void> {
             Logger.info(`spawning numberhuman in channel ${channel.id}`);
-            await channel.send("test");
+            const number = await spawnNumberhuman(channel);
+            if (number.isOk()) {
+              const okNumber = number.unwrap();
+              const handler = async (message: OmitPartialGroupDMChannel<Message>) => {
+                if (message.channelId !== channel.id || message.author.bot) return;
+                if (handlePlayerGuess(message, okNumber)) {
+                  client.off("messageCreate", handler);
+
+                  // @ts-expect-error: assertion fails for some reason even though the bot can only
+                  // be installed in a guild
+                  const user = await getUser(message.author.id, message.guildId);
+                  Logger.debug(`tried looking up user ${message.author.id} (found: ${user ? "true" : "false"})`);
+
+                  if (user) {
+                    Logger.info(`user already exists, adding the numberhuman to their collection`);
+                    // update the player stats first...
+                    user.numberhumansGuessed.push(okNumber.uuid);
+                    if (user.numberhumansGuessedUnique.includes(okNumber.uuid)) {
+                      await message.reply(joinStringArray([
+                        `hey, you managed to ~~kidnap~~ catch **${okNumber.number}**!`,
+                      ]));
+                    } else {
+                      user.numberhumansGuessedUnique.push(okNumber.uuid);
+                      await message.reply(joinStringArray([
+                        `hey, you managed to ~~kidnap~~ catch **${okNumber.number}**!`,
+                        "woah is that a new numberhuman you caught??",
+                      ]));
+                    }
+                    // and saves.
+                    await user.save();
+                  } else {
+                    Logger.info(`user not found, creating user and adding the numberhuman`);
+                    // @ts-expect-error: assertion fails for some reason even though the bot can only
+                    // be installed in a guild
+                    const newUser = await createUser(message.author.id, message.guildId);
+                    newUser.numberhumansGuessed.push(okNumber.uuid);
+                    // this is a fresh new profile which means it is guaranteed to have zero unique guesses.
+                    // so we can add it without checking.
+                    newUser.numberhumansGuessedUnique.push(okNumber.uuid);
+                    await message.reply(joinStringArray([
+                      `hey, you managed to ~~kidnap~~ catch **${okNumber.number}**!`,
+                      `i've also created a profile for you with that numberhuman.`,
+                    ]));
+                    await newUser.save();
+                  }
+                }
+              };
+              client.on("messageCreate", handler);
+            }
           },
         });
         await interaction.reply(`added channel <#${channel.id}>.`);
