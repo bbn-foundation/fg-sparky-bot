2653a1f33cbcc9170662ff485b4119e5dcbd328a
Skylafalls <triplettran56@gmail.com>
2025-12-20 19:20:29 -0500

    it works!!!!!!!
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- src/numberdex/utils.ts
+++ src/numberdex/utils.ts
@@ -6,11 +6,42 @@
  */
 import { DiscordjsError, type SendableChannels } from "discord.js";
 import { Err, Ok, type Result } from "rust-optionals";
+import numberhumans from "../../numbers/numberhumans.json" with { type: "json" };
 
-export async function spawnNumberhuman(id: string, channel: SendableChannels): Promise<Result<void, Error>> {
+export type Rarity = "common" | "uncommon" | "rare" | "epic" | "legendary" | "mythical";
+
+export interface NumberhumanInfo {
+  number: string;
+  hashedNumber: string;
+  image: string;
+  uuid: string;
+  rarity: Rarity;
+}
+
+function getRandomRarity(): Rarity {
+  return "common";
+}
+
+export function findRandomNumber(): NumberhumanInfo {
+  const rarity = getRandomRarity();
+  const humanPool = numberhumans[rarity];
+  const randomIndex = Math.floor(Math.random() * humanPool.length);
+  const number = humanPool[randomIndex];
+  // Uh yeah same here
+  return {
+    number: number!.name,
+    hashedNumber: number!.hashedName,
+    image: number!.image,
+    rarity,
+    uuid: number!.uuid,
+  };
+}
+
+export async function spawnNumberhuman(channel: SendableChannels): Promise<Result<NumberhumanInfo, Error>> {
+  const numberhuman = findRandomNumber();
   try {
-    await channel.send({ content: "hello", files: [`numbers/humans/${id}.jpg`] });
-    return Ok();
+    await channel.send({ content: "hello", files: [numberhuman.image] });
+    return Ok(numberhuman);
   } catch (err) {
     if (err instanceof DiscordjsError) return Err(err);
     return Err("unknown error");
