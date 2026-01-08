7c868d332c42934d64b5d88d4722de61829be548
Skylafalls <triplettran56@gmail.com>
2026-01-08 17:31:29 -0500

    fix eslint
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,26 +1,31 @@
 import stylistic from "@stylistic/eslint-plugin";
 import oxlint from "eslint-plugin-oxlint";
 import { type Config, defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
 
 const _default: Config[] = defineConfig(
-  globalIgnores(["node_modules", "dist"]),
+  globalIgnores([
+    "node_modules",
+    "**/node_modules",
+    "**/dist",
+    "dist",
+    "build"
+  ]),
+  tseslint.configs.recommendedTypeCheckedOnly,
+  tseslint.configs.disableTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
   stylistic.configs.customize({
     braceStyle: "1tbs",
     semi: true,
     quotes: "double",
   }),
   oxlint.buildFromOxlintConfigFile("./.oxlintrc.json"),
-  {
-    rules: {
-      // See https://github.com/eslint/eslint/issues/20272
-      "@typescript-eslint/unified-signatures": "off",
-      // Namespaces are cool
-      "@typescript-eslint/no-namespace": "off",
-      "@typescript-eslint/no-misused-promises": ["error", {
-        checksVoidReturn: false,
-      }],
-    },
-  },
 );
 
 export default _default;
