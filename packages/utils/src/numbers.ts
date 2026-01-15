cb9eff5882188f8ca8a30feabe2a630bfad928c9
Skylafalls <triplettran56@gmail.com>
2026-01-15 18:24:43 -0500

    make random number function inclusive
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- packages/utils/src/numbers.ts
+++ packages/utils/src/numbers.ts
@@ -18,7 +18,7 @@ interface DifficultyTokenMap {
 // please don't mind the `as never` cast, that's just typescript being stupid
 // oxlint-disable no-unsafe-type-assertion
 export function getGainFromDifficulty<T extends keyof DifficultyTokenMap>(
-  difficulty: T,
+  difficulty: T
 ): DifficultyTokenMap[T] {
   switch (difficulty) {
     case "easy": {
@@ -37,7 +37,9 @@ export function getGainFromDifficulty<T extends keyof DifficultyTokenMap>(
 }
 // oxlint-enable no-unsafe-type-assertion
 
-export function ordinalOf(number: number): `${number}${"st" | "nd" | "rd" | "th"}` {
+export function ordinalOf(
+  number: number
+): `${number}${"st" | "nd" | "rd" | "th"}` {
   const j = number % 10,
     k = number % 100;
   if (j === 1 && k !== 11) {
@@ -52,11 +54,9 @@ export function ordinalOf(number: number): `${number}${"st" | "nd" | "rd" | "th"
   return `${number}th`;
 }
 
-export function countEntriesUnique<T extends Record<Difficulties, { uuid: string }[]>>(
-  numbers: T,
-  difficulty: Difficulties,
-  entries: string[],
-): number {
+export function countEntriesUnique<
+  T extends Record<Difficulties, { uuid: string }[]>
+>(numbers: T, difficulty: Difficulties, entries: string[]): number {
   const filtered = numbers[difficulty].filter((entry) => {
     for (const uuid of entries) {
       if (entry.uuid === uuid) return true;
@@ -66,11 +66,9 @@ export function countEntriesUnique<T extends Record<Difficulties, { uuid: string
   return filtered.length;
 }
 
-export function countEntriesTotal<T extends Record<Difficulties, { uuid: string }[]>>(
-  numbers: T,
-  difficulty: Difficulties,
-  entries: string[],
-): number {
+export function countEntriesTotal<
+  T extends Record<Difficulties, { uuid: string }[]>
+>(numbers: T, difficulty: Difficulties, entries: string[]): number {
   const filtered = entries.filter((uuid) => {
     for (const entry of numbers[difficulty]) {
       if (uuid === entry.uuid) return true;
@@ -80,11 +78,9 @@ export function countEntriesTotal<T extends Record<Difficulties, { uuid: string 
   return filtered.length;
 }
 
-export function countHumansUnique<T extends Record<Rarities, { uuid: string }[]>>(
-  numberhumans: T,
-  rarity: Rarities,
-  entries: string[],
-): number {
+export function countHumansUnique<
+  T extends Record<Rarities, { uuid: string }[]>
+>(numberhumans: T, rarity: Rarities, entries: string[]): number {
   const filtered = numberhumans[rarity].filter((entry) => {
     for (const uuid of entries) {
       if (entry.uuid === uuid) return true;
@@ -94,11 +90,9 @@ export function countHumansUnique<T extends Record<Rarities, { uuid: string }[]>
   return filtered.length;
 }
 
-export function countHumansTotal<T extends Record<Rarities, { uuid: string }[]>>(
-  numberhumans: T,
-  rarity: Rarities,
-  entries: string[],
-): number {
+export function countHumansTotal<
+  T extends Record<Rarities, { uuid: string }[]>
+>(numberhumans: T, rarity: Rarities, entries: string[]): number {
   const filtered = entries.filter((uuid) => {
     for (const entry of numberhumans[rarity]) {
       if (uuid === entry.uuid) return true;
@@ -109,5 +103,5 @@ export function countHumansTotal<T extends Record<Rarities, { uuid: string }[]>>
 }
 
 export function getRandomRange(min: number, max: number): number {
-  return Math.random() * (max - min) + min;
+  return Math.random() * (max - min + 1) + min;
 }
