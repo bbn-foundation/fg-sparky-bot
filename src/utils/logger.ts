533094557894dcc72c382b8f162dfdb95f88ba3b
Skylafalls <triplettran56@gmail.com>
2026-01-27 14:32:46 -0500

    fix(misc): some fixups
    Signed-off-by: Skylafalls <triplettran56@gmail.com>

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -9,48 +9,54 @@ import { loggerFormatter } from "./formatter.ts";
 
 export const Logger = {
   loglevel: 0,
-  debug(str: string): void {
+  debug(str: string, ...values: string[]): void {
     if (this.loglevel > 0) return;
-    console.debug(`[${loggerFormatter.format(Date.now())}] ${chalk.blue("[DEBUG]")}: %s`, str);
+    console.debug(
+      `[${loggerFormatter.format(Date.now())}] ${chalk.blue("[DEBUG]")}: ${str}`,
+      ...values,
+    );
   },
 
-  info(str: string): void {
+  info(str: string, ...values: string[]): void {
     if (this.loglevel > 1) return;
-    console.log(`[${loggerFormatter.format(Date.now())}] ${chalk.grey("[INFO]")}: %s`, str);
+    console.log(
+      `[${loggerFormatter.format(Date.now())}] ${chalk.grey("[INFO]")}: ${str}`,
+      ...values,
+    );
   },
 
-  notice(str: string): void {
+  notice(str: string, ...values: string[]): void {
     if (this.loglevel > 2) return;
     console.log(
-      `[${loggerFormatter.format(Date.now())}] ${chalk.whiteBright("[NOTICE]")}: %s`,
-      str,
+      `[${loggerFormatter.format(Date.now())}] ${chalk.whiteBright("[NOTICE]")}: ${str}`,
+      ...values,
     );
   },
 
-  warn(str: string): void {
+  warn(str: string, ...values: string[]): void {
     if (this.loglevel > 3) return;
     console.warn(
-      `[${loggerFormatter.format(Date.now())}] ${chalk.yellowBright("[WARN]")}: %s`,
-      str,
+      `[${loggerFormatter.format(Date.now())}] ${chalk.yellowBright("[WARN]")}: ${str}`,
+      ...values,
     );
   },
 
-  error(str: string): void {
+  error(str: string, ...values: string[]): void {
     if (this.loglevel > 4) return;
-    console.error(`[${loggerFormatter.format(Date.now())}] ${chalk.redBright("[ERROR]")}: %s`, str);
+    console.error(`[${loggerFormatter.format(Date.now())}] ${chalk.redBright("[ERROR]")}: ${str}`, ...values);
   },
 
-  crit(str: string): void {
+  crit(str: string, ...values: string[]): void {
     console.error(
-      `[${loggerFormatter.format(Date.now())}] ${chalk.magentaBright("[CRIT]")}: %s`,
-      str,
+      `[${loggerFormatter.format(Date.now())}] ${chalk.magentaBright("[CRIT]")}: ${str}`,
+      ...values,
     );
   },
 
-  success(str: string): void {
+  success(str: string, ...values: string[]): void {
     console.log(
-      `[${loggerFormatter.format(Date.now())}] ${chalk.greenBright("[SUCCESS]")}: %s`,
-      str,
+      `[${loggerFormatter.format(Date.now())}] ${chalk.greenBright("[SUCCESS]")}: ${str}`,
+      ...values,
     );
   },
 };
